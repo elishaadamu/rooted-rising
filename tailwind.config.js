7f50dd443ce56f372c0c220c409c255502467533
Adamu Elisha Inuwa <40290364+elishadamu97@users.noreply.github.com>
2025-02-22 15:41:15 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./_layouts/**/*.html", "./_includes/**/*.html", "./**/*.markdown"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
